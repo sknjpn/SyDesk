3f03b18b554d3f163f0b5ea791d43b6f6d0d9acc
SKNJPN <sknjpn@gmail.com>
2020-01-01 10:53:21 +0900

    コンストラクタの追加

--- Command.h
+++ Command.h
@@ -8,7 +8,15 @@ class Command
 	short	m_parameters[4];
 
 public:
-	
+	Command(char code, short param0, short param1, short param2, short param3)
+		: m_code(code)
+	{
+		m_parameters[0] = param0;
+		m_parameters[1] = param1;
+		m_parameters[2] = param2;
+		m_parameters[3] = param3;
+	}
+
 	std::string asSendBuffer() const;
 };
 
