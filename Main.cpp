06486d5f240c4a9f12353b925fc9dbebe8f7bef6
SKNJPN <sknjpn@gmail.com>
2020-01-01 11:44:07 +0900

    テスト修正

--- Main.cpp
+++ Main.cpp
@@ -9,7 +9,29 @@ void Main()
 
 	if (!communicator.connect(U"COM16")) return;
 
-	
+	{
+		short preState = 0b10000;
+		while (System::Update())
+		{
+			short nowState = 0b10000;
+			if (KeyLeft.pressed()) nowState += 0b0001;
+			if (KeyUp.pressed()) nowState += 0b0010;
+			if (KeyRight.pressed()) nowState += 0b0100;
+			if (KeyDown.pressed()) nowState += 0b1000;
+
+			if (preState != nowState)
+			{
+				communicator.addCommand('K', nowState, 0, 0, 0);
+
+				preState = nowState;
+			}
+
+			communicator.update();
+
+			if (KeyEnter.down()) break;
+		}
+	}
+
 	/*communicator.addCommand('D', 1000, 2000, 500, 4000);
 	communicator.addCommand('D', -1000, 2000, 500, 4000);
 	communicator.addCommand('D', -1000, 2000, -500, 4000);
