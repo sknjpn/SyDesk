c21c9b379dbb67a249713b413daa9ca5d53af063
SKNJPN <sknjpn@gmail.com>
2020-01-01 11:37:41 +0900

    Polygonを使ったテストに修正

--- RouteGenerator.cpp
+++ RouteGenerator.cpp
@@ -2,7 +2,16 @@
 
 Array<Command> RouteGenerator::getCommands() const
 {
+	Array<Vec2>	m_route;
 
+	for (const auto& polygon : m_multiPolygon)
+		m_route.append(polygon.outer());
 
-	return Array<Command>();
+	Array<Command> commands;
+	for (const auto& p : m_route)
+	{
+		commands.emplace_back('P', p.x * 10, 2000, p.y * 10, 2000);
+	}
+
+	return commands;
 }
