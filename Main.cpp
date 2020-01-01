8b80eec4282e2c7c37884e2fb76e5bbafd1764fc
SKNJPN <sknjpn@gmail.com>
2020-01-01 10:31:17 +0900

    通信テストの実行

--- Main.cpp
+++ Main.cpp
@@ -2,5 +2,28 @@
 
 void Main()
 {
+	Serial serial;
 
+	if (!serial.open(U"COM16", 115200)) return;
+
+	{
+		std::string sendBuffer;
+
+		short px = 1000;
+		short sx = 3000;
+		short py = 1000;
+		short sy = 3000;
+
+		sendBuffer.push_back('D');
+		sendBuffer.push_back(char(px >> 8));
+		sendBuffer.push_back(char(px));
+		sendBuffer.push_back(char(sx >> 8));
+		sendBuffer.push_back(char(sx));
+		sendBuffer.push_back(char(py >> 8));
+		sendBuffer.push_back(char(py));
+		sendBuffer.push_back(char(sy >> 8));
+		sendBuffer.push_back(char(sy));
+
+		serial.write(sendBuffer.c_str(),sendBuffer.size());
+	}
 }
