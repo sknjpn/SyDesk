f039a913b68d382ed03bc984f89d482224ef6af3
SKNJPN <sknjpn@gmail.com>
2020-01-01 10:42:03 +0900

    コマンド追加 メンバの追加

--- Communicator.h
+++ Communicator.h
@@ -9,4 +9,8 @@ class Communicator
 
 public:
 	bool	connect(String portname) { return m_serial.open(portname, 115200); }
+
+	void	addCommand(const Command& command) { m_commands.emplace_back(command); }
+	template<typename... Args>
+	bool	addCommand(Args&&... args) { m_commands.emplace_back(args); }
 };
