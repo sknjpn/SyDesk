2353db5307d5b2ce5d51751843f9b08f017ad07c
SKNJPN <sknjpn@gmail.com>
2020-01-01 11:30:47 +0900

    メンバ追加

--- Communicator.h
+++ Communicator.h
@@ -11,6 +11,7 @@ class Communicator
 public:
 	bool	connect(String portname) { return m_serial.open(portname, 115200); }
 
+	void	addCommands(const Array<Command>& commands) { m_commands.append(commands); }
 	void	addCommand(const Command& command) { m_commands.emplace_back(command); }
 	void	addCommand(char code, short param0, short param1, short param2, short param3) { m_commands.emplace_back(code, param0, param1, param2, param3); }
 
