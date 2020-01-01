76a476a9e6a9c64f2bbca5756f17406fb14e67f2
SKNJPN <sknjpn@gmail.com>
2020-01-01 10:55:35 +0900

    返り値の修正

--- Communicator.h
+++ Communicator.h
@@ -12,7 +12,7 @@ public:
 	bool	connect(String portname) { return m_serial.open(portname, 115200); }
 
 	void	addCommand(const Command& command) { m_commands.emplace_back(command); }
-	bool	addCommand(char code, short param0, short param1, short param2, short param3) { m_commands.emplace_back(code, param0, param1, param2, param3); }
+	void	addCommand(char code, short param0, short param1, short param2, short param3) { m_commands.emplace_back(code, param0, param1, param2, param3); }
 
 	void	update();
 };
