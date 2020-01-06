b75eaa92f2678c4d1b59fcc7279db326dfc24def
SKNJPN <sknjpn@gmail.com>
2020-01-06 12:18:22 +0900

    表示の設定

--- MainViewer_SerialSelector.cpp
+++ MainViewer_SerialSelector.cpp
@@ -2,8 +2,22 @@
 
 void MainViewer::SerialSelector::init()
 {
+	setViewerSize(640, 480);
+	setViewerPosInLocal(Scene::Center() - getViewerSize() / 2.0);
 }
 
 void MainViewer::SerialSelector::update()
 {
+	// Serial List
+	{
+		static Font font(24);
+		const auto serialPorts = System::EnumerateSerialPorts();
+
+		for (const auto& serialPort : serialPorts)
+		{
+			font(serialPort.port, U"--", serialPort.hardwareID, U"--", serialPort.description).draw();
+
+			moveDrawPos(0, 32);
+		}
+	}
 }
