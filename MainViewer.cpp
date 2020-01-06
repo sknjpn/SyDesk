6efc2e6557b92a5a5cd5438f10ea419542987026
SKNJPN <sknjpn@gmail.com>
2020-01-06 12:14:30 +0900

    Main処理追加

--- MainViewer.cpp
+++ MainViewer.cpp
@@ -1,7 +1,18 @@
 ﻿#include "MainViewer.h"
 
+// ワークフロー
+// 1, SerialSelector
+// 2, OriginAdjustment
+// 3, Work
+
 void MainViewer::init()
 {
+	addChildViewer<Workspace>();
+	addChildViewer<CutSetting>();
+	addChildViewer<SerialViewer>();
+
+	// 最初のみ
+	addChildViewer<SerialSelector>();
 }
 
 void MainViewer::update()
